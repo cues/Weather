4c59499a489fcebf430ee6bd6138f9640285bc1e
Erroll Alfredo Antao <errollalfredoantao@Errolls-MacBook-Air.local>
2016-08-28 03:58:16 +0530

    new changes

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,7 +4,7 @@ var Examples = React.createClass({
   render: function () {
     return(
       <div>
-        Examples component
+        Examples component !
       </div>
     );
   }
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,16 +1,24 @@
 var React = require('react');
 
 
-var WeatherMessage = React.createClass({
+// var WeatherMessage = React.createClass({
+//
+//   render: function () {
+//     var {temp,location} = this.props;
+//     return(
+//       <div>
+//         <h2>it is {temp} degrees  in {location}</h2>
+//       </div>
+//     );
+//   }
+// })
 
-  render: function () {
-    var {temp,location} = this.props;
-    return(
-      <div>
-        <h2>it is {temp} degrees  in {location}</h2>
-      </div>
-    );
-  }
-})
-
+var WeatherMessage = (props) => {
+  var {temp,location} = props;
+  return(
+    <div>
+      <h2>it is {temp} degrees  in {location}</h2>
+    </div>
+  );
+}
 module.exports = WeatherMessage;
