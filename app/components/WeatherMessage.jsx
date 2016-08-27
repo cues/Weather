da1630a268287bee89cb0fb9183e628d248182c8
Erroll Alfredo Antao <errollalfredoantao@Errolls-MacBook-Air.local>
2016-08-28 04:52:39 +0530

    added message

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -14,10 +14,11 @@ var React = require('react');
 // })
 
 var WeatherMessage = (temp,location) => {
-  
+
   return(
     <div>
       <h2>it is {temp} degrees  in {location}</h2>
+      <div>Why is the temperature like this?</div>
     </div>
   );
 }
