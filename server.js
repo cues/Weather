0aa2ee1fb3ccb5b1a7c268c99b8e15e4a47614d1
Erroll Alfredo Antao <errollalfredoantao@Errolls-MacBook-Air.local>
2016-08-28 06:25:20 +0530

    prep repo for heroku

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ var express = require('express');
 //create the app
 
 var app = express();
+const PORT = process.env.PORT || 1707; /*HEROKU */
+
+// CHECK IF THE TRAFFIC IS ON HTTP
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 
 // server to use , allows you to use functionality
 app.use(express.static('public'));
 
-app.listen(1707, function(){
-  console.log('Expres server is up on port 1707');
+app.listen(PORT, function(){
+  console.log('Expres server is up on port' + PORT);
 });
