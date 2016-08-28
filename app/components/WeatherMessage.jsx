cba85a82aa60fd4a7d8492fe1e84f36f288b9f14
Erroll Alfredo Antao <errollalfredoantao@Errolls-MacBook-Air.local>
2016-08-28 06:33:26 +0530

    message

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,25 +1,25 @@
 var React = require('react');
 
 
-// var WeatherMessage = React.createClass({
+var WeatherMessage = React.createClass({
+
+  render: function () {
+    var {temp,location} = this.props;
+    return(
+      <div>
+        <h2>it is {temp} degrees  in {location}</h2>
+      </div>
+    );
+  }
+})
+
+// var WeatherMessage = (temp,location) => {
 //
-//   render: function () {
-//     var {temp,location} = this.props;
-//     return(
-//       <div>
-//         <h2>it is {temp} degrees  in {location}</h2>
-//       </div>
-//     );
-//   }
-// })
-
-var WeatherMessage = (temp,location) => {
-
-  return(
-    <div>
-      <h2>it is {temp} degrees  in {location}</h2>
-      <div>Why is the temperature like this?</div>
-    </div>
-  );
-}
+//   return(
+//     <div>
+//       <h2>it is {temp} degrees  in {location}</h2>
+//       <div>Whycis the temperature like this?</div>
+//     </div>
+//   );
+// }
 module.exports = WeatherMessage;
